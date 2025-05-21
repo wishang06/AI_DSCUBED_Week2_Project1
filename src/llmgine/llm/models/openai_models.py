3dd161c3f5549fe92201943384c33b4be66f6667
Nathan Luo <112300416+nathan-luo@users.noreply.github.com>
2025-05-21 18:43:00 +1000

    new models (#9)

--- src/llmgine/llm/models/openai_models.py
+++ src/llmgine/llm/models/openai_models.py
@@ -30,6 +30,88 @@ from llmgine.llm.providers import Providers
 dotenv.load_dotenv(override=True)
 
 
+class Gpt41:
+    """
+    The latest GPT-4.1 model.
+    """
+
+    def __init__(self, provider: Providers) -> None:
+        self.generate = None
+        self.model = "gpt-4.1-2025-04-14"
+        self.api_key = os.getenv("OPENAI_API_KEY")
+        self.provider = self.__getProvider(provider)
+
+    def __getProvider(self, provider: Providers) -> OpenAIProvider:
+        """Get the provider and set the generate method."""
+        if provider == Providers.OPENROUTER:
+            self.generate = self._generate_openrouter
+            return OpenRouterProvider(self.api_key, self.model)
+        elif provider == Providers.OPENAI:
+            self.generate = self._generate_openai
+            return OpenAIProvider(self.api_key, self.model)
+        else:
+            raise ValueError(
+                f"Provider {provider} not supported for {self.__class__.__name__}"
+            )
+
+    def _generate_openai(
+        self,
+        messages: List[Dict],
+        tools: Optional[List[Dict]] = None,
+        tool_choice: Union[Literal["auto", "none", "required"], Dict] = "auto",
+        parallel_tool_calls: Optional[bool] = None,
+        temperature: Optional[float] = None,
+        max_completion_tokens: int = 5068,
+        response_format: Optional[Dict] = None,
+        test: bool = False,
+        **kwargs: Any,
+    ) -> OpenAIResponse:
+        """
+        This method will hardcode a group of default parameters for the OpenAI provider for the GPT-4o Mini model.
+        """
+        # Update the parameters with the ones provided in the kwargs.
+        return self.provider.generate(
+            messages=messages,
+            temperature=temperature,
+            tools=tools,
+            tool_choice=tool_choice,
+            parallel_tool_calls=parallel_tool_calls,
+            max_completion_tokens=max_completion_tokens,
+            response_format=response_format,
+            reasoning_effort=None,
+            test=test,
+            **kwargs,
+        )
+
+    def _generate_openrouter(
+        self,
+        messages: List[Dict],
+        tools: Optional[List[Dict]] = None,
+        tool_choice: Union[Literal["auto", "none", "required"], Dict] = "auto",
+        parallel_tool_calls: Optional[bool] = None,
+        temperature: Optional[float] = None,
+        max_completion_tokens: int = 5068,
+        response_format: Optional[Dict] = None,
+        reasoning_effort: Optional[Literal["low", "medium", "high"]] = None,
+        test: bool = False,
+        **kwargs: Any,
+    ) -> OpenAIResponse:
+        """
+        This method will construct a default group of parameters for the OpenRouter provider.
+        """
+        return self.provider.generate(
+            messages=messages,
+            temperature=temperature,
+            tools=tools,
+            tool_choice=tool_choice,
+            max_completion_tokens=max_completion_tokens,
+            response_format=response_format,
+            reasoning_effort=reasoning_effort,
+            test=test,
+            **kwargs,
+        )
+
+
 class Gpt41Mini:
     """
     The latest GPT-4.1 Mini model.
